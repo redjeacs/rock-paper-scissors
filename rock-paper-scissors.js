b2f0b47fd82f27884c0ac3fb02e50e49f2d98202
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-11 12:10:19 +0800

    update function getComputerChoice
    Used math.random to generate a random number
    between 0-1. Using if...else functions to return
    rock, paper, or scissors as choice if math.random
    generates a number between 0-1/3, 1/3-2/3, and
    2/3-1 respectively.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,15 @@
-console.log(" World!")
+function getComputerChoice() {
+  let rock = 'rock';
+  let paper = 'paper';
+  let scissors = 'scissors';
+  let choice = Math.random();
+  if (choice < 1/3) {
+    return choice = rock;
+  }
+  else if (choice < 2/3) {
+    return choice = paper;
+  }
+  else {
+    return choice = scissors;
+  }
+}
