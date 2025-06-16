eb3e8d795bd8fb1ad662f6ebe1287c47305f689b
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-16 14:28:40 +0800

    run playRound when buttons are clicked
    added eventlisteners to all three buttons that
    will play out 1 round of rps on click and display
    running score according to results changed all
    console logs to DOM elements for display on the
    page

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,5 @@
 let humanScore = 0;
-let computerScore =0;
+let computerScore = 0;
 
 
 const rockBtn = document.createElement('button');
@@ -10,32 +10,40 @@ body.appendChild(rockBtn);
 body.appendChild(paperBtn);
 body.appendChild(scissorsBtn);
 
-function playGame () {
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    console.log('Score - You: ' + humanScore + ' vs. Computer: ' + computerScore);
-  }
+const results = document.createElement('div');
+body.appendChild(results);
+results.textContent = '';
+const score = document.createElement('div');
+body.appendChild(score);
+score.textContent = ''
+
+rockBtn.addEventListener('click', () => btnClicked('rock'));
+paperBtn.addEventListener('click', () => btnClicked('paper'));
+scissorsBtn.addEventListener('click', () => btnClicked('scissors'));
+
+function btnClicked(humanSelection) {
+  const computerSelection = getComputerChoice();
+  playRound(humanSelection, computerSelection);
 }
 
 function playRound(humanChoice, computerChoice) {
   humanChoice.toLowerCase();
   if (humanChoice === computerChoice) {
-    console.log('It\'s a Draw! Both chose ' + humanChoice);
+    results.textContent = 'It\'s a Draw! Both chose ' + humanChoice;
   }
   else if (
-  humanChoice === 'rock' && computerChoice === 'scissors' || 
-  humanChoice === 'paper' && computerChoice === 'rock' || 
-  humanChoice === 'scissors' && computerChoice === 'paper'
+    humanChoice === 'rock' && computerChoice === 'scissors' || 
+    humanChoice === 'paper' && computerChoice === 'rock' || 
+    humanChoice === 'scissors' && computerChoice === 'paper'
   ) {
-    console.log('You Win! ' + humanChoice + ' beats ' + computerChoice);
+    results.textContent = 'You Win! ' + humanChoice + ' beats ' + computerChoice;
     humanScore++;
   }
   else {
-    console.log('You Lose! ' + humanChoice + ' loses to ' + computerChoice);
+    results.textContent = 'You Lose! ' + humanChoice + ' loses to ' + computerChoice;
     computerScore++;
   }
+  score.textContent = 'Score - Player: ' + humanScore + ' vs. Computer: ' + computerScore;
 }
 
 function getComputerChoice() {
@@ -49,9 +57,4 @@ function getComputerChoice() {
   else {
     return 'scissors';
   }
-}
-
-function getHumanChoice() {
-  return prompt('Make your Choice');
-  
 }
